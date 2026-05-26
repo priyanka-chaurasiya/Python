64ef4e0994429195abc6383abd87959e4336445b
Priyanka Chaurasiya <priyankachaurasiya6509@gmail.com>
2026-05-26 14:25:48 +0530

    Add files via upload

---
+++ chapter 6/loopsBasic.py
@@ -0,0 +1,19 @@
+# while loop and for loop
+# print the name 100 times 
+
+num = 1
+
+while num<=5:
+    print("Priyanka Chaurasiya")
+    num= num+1
+
+print("Now we are out of the while loop")
+
+'''
+num= 1 Priyanka Chaurasiya
+2 Priyanka Chaurasiya
+3 Priyanka Chaurasiya
+4 Priyanka Chaurasiya
+num= 5 Priyanka Chaurasiya
+"Now we are out of the while loop
+'''
