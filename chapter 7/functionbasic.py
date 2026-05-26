a98bd386266b41c529a07bcfa1ee5dfeec7f23f1
Priyanka Chaurasiya <priyankachaurasiya6509@gmail.com>
2026-05-26 14:29:05 +0530

    Add files via upload

---
+++ chapter 7/functionbasic.py
@@ -0,0 +1,21 @@
+# Function Definition with Parameters
+def average(a=10,b=20):
+    averageValue= (a+b)/2
+    print(averageValue)
+
+
+# Function Calling with Arguments 
+average(5, 10)
+average(7, 10)
+average(80, 98)
+average()
+
+# Write a function show_age(name, age) that prints: "Saumya Singh is 21
+# years old."
+
+def show_age(name="Priyanka Chaurasiya", age= 25):
+    print(f"{name} is {age} years old")
+
+show_age("Priyanka Chaurasiya", 25)
+show_age()
+show_age("Divya", 19)
